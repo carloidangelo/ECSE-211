c9195d1b47123289e12eae86b3e953476d4977bd
MoeSamee <47723277+MoeSamee@users.noreply.github.com>
2019-02-19 13:19:59 -0500

    Update CanLocator.java

--- src/ca/mcgill/ecse211/lab5/CanLocator.java
+++ src/ca/mcgill/ecse211/lab5/CanLocator.java
@@ -13,24 +13,83 @@ import lejos.robotics.SampleProvider;
 
 public class CanLocator {
 
-  private Odometer odo;
+	private Odometer odo;
 	private Navigation navigation;
 	private static int FORWARD_SPEED = 100;
-	double [] lightData;
+	double [] lightData; //"angles"
+	
+	private int LLx, LLy, UPx, URy,TR,SC;
 	
 	private SampleProvider colorSensor;
 	private float[] colorData;
-	private float previousValue = 0;
-	private float currentValue = 0;
 	
-	public CanLocator(Odometer odo, SampleProvider colorSensor, float[] colorData) {
-		this.odo = odo;
-		this.navigation = new Navigation(odo);
-		this.lightData = new double [4];
+	public CanLocator(Odometer odo, SampleProvider colorSensor, float[] colorData,
+			 int SC, int TR, int LLx, int LLy, int UPx, int URy, ) {
+	this.odo = odo;
+	this.navigation = new Navigation(odo);
+	this.lightData = new double [4]; //"angles"
 		
-		this.colorSensor = colorSensor;
-		this.colorData = colorData;
+	this.colorSensor = colorSensor;
+	this.colorData = colorData;
+	
+	this.SC = SC;
+	this.TR = TR;	
+	this.LLx = LLx;
+	this.LLy = LLy;
+	this.UPx = UPx;
+	this.URy = URy;
 	}
-  
+	
+	public void RunLocator(){
+	
+		
+		
+		switch(TR){
+			case 1: //blue can
+				
+				break;
+			case 2: //green can
+				
+				break;
+			case 3: //yellow can
+				
+				break;
+			case 4:	//red can
+				
+				break;
+		}
+	}	
+	
+	/** CheckSC() checks the starting corner of the EV3. Depending on where
+	 *  it was placed to begin, the EV3 will make its way to the search zone
+	 * and corrects its angle to follow the convention given in Lab 5.
+	 */
+	
+	private void CheckSC(){
+		
+		switch(this.SC){ //NOTE, check if splitting LLx LLy is more accurate
+			case 0: 
+				//current position is (1,1)
+				//go to LLy, turn 90 RIGHT
+				//go to LLx, turn 90 LEFT
+				break;
+			case 1: 
+				//current position is (7,1)
+				//go to LLx, then turn 90 RIGHT and reset angle to zero
+				//go to LLy
+				break;
+			case 2: 
+				//current position is (7,7)
+				//go to LLy, turn 90 RIGHT
+				//go to LLx, turn 90 RIGHT and reset angle to zero
+				break;
+			case 3:	
+				//current position is (1,7)
+				//go to LLy, turn 90 LEFT
+				//go to LLx, turn 90 LEFT and reset angle to zero
+				break;
+		}
+		
+	}  
   
 }
