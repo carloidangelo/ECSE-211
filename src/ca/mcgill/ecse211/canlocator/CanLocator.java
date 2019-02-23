a9629cff7b5a6c79ccf18ac14278d080957245bd
MoeSamee <47723277+MoeSamee@users.noreply.github.com>
2019-02-23 15:56:31 -0500

    Update CanLocator.java

--- src/ca/mcgill/ecse211/canlocator/CanLocator.java
+++ src/ca/mcgill/ecse211/canlocator/CanLocator.java
@@ -9,19 +9,29 @@ import lejos.robotics.SampleProvider;
 public class CanLocator {
 
 	private Odometer odo;
-	private static int FORWARD_SPEED = 100;
-	double [] lightData; //"angles"
-	
-	private int TR;
 	private Navigation navigator;
 	private ColorClassification csFront;
 	private SampleProvider usDistance;
 	private float[] usData;
 	
+	private static int FORWARD_SPEED = 100;
+	private static final double TILE_SIZE = 30.48;
+	private static final double CAN_DISTANCE = 27.00;
+	
+	/**
+	*TR is the variable that stores the integer defining the target can color we are looking
+	*cX,cY store the changes in x and why distance so that the EV3 knows which ways to turn.
+	*i.e if cX is zero but cY is a positive number, then the EV3 is traveling straight
+	*up the search zone.
+	*/
+	
+	private int TR;
+	private int cX,xY = 0;
+	private int count = 0;
+	
 	public CanLocator(ColorClassification csFront, SampleProvider usDistance, float[] usData, 
 			Navigation navigator, int TR) throws OdometerExceptions {
 		odo = Odometer.getOdometer();
-		this.lightData = new double [4]; //"angles"
 		this.usDistance = usDistance;
 		this.usData = usData;
 		this.csFront = csFront;
@@ -58,7 +68,34 @@ public class CanLocator {
 		}*/
 	}	
 	
-	private void goToNext() {
+	private boolean checkForCan(){
+		
+		if (!(count == 0)) {
+			
+			
+			
+			count++;
+		}
+		
+		//read sensor and see if can detected in a TILE_SIZE
+		//if yes, move towards can
+		
+	}
+	
+	private void goToNext() { //navigator.travelTo(LLx,1);
+				
+		//go forward one tile
+		//turn 90 deg right
+		
+	}
+	
+	private void borderToUR() {
+		
+		//
+		
+	}
+	
+	private void outsideToUR() {
 		
 		//
 		
@@ -75,16 +112,12 @@ public class CanLocator {
 		//
 		
 	}
-	
-	private void outsideDodge() {
 		
-		//
-		
-	}
-	
 	private int readUSDistance() {
+		
 		usDistance.fetchSample(usData, 0);
 		return (int) (usData[0] * 100);
+		
 	}
   
 }
