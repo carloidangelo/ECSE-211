e8fc6638aa0fe79b4170dca31ab717f57169927b
MoeSamee <47723277+MoeSamee@users.noreply.github.com>
2019-04-01 17:31:08 -0400

    Update Clamp.java

--- src/ca/mcgill/ecse211/model/Clamp.java
+++ src/ca/mcgill/ecse211/model/Clamp.java
@@ -2,6 +2,12 @@ package ca.mcgill.ecse211.model;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
+/**
+ * This class is responsible for driving the clamp motor to load and unload the can.
+ * @author Carlo D'Angelo
+ * 
+ */
+
 public class Clamp {
 	private static final int ROTATE_AMOUNT = 198;
 	private static final int ROTATION_SPEED = 75;
@@ -12,12 +18,18 @@ public class Clamp {
 		this.clampMotor = clampMotor;
 	}
 	
+	/**
+	 * grabCan() rotates the motor clockwise to hold onto the can.
+	 */
 	public void grabCan() {
 		clampMotor.setAcceleration(1000);
 		clampMotor.setSpeed(ROTATION_SPEED);
 		clampMotor.rotate(ROTATE_AMOUNT);
 	}
 	
+	/**
+	 * offloadCan() rotates the motor counter clockwise to let the can go.
+	 */
 	public void offloadCan() {
 		clampMotor.setAcceleration(1000);
 		clampMotor.setSpeed(ROTATION_SPEED);
