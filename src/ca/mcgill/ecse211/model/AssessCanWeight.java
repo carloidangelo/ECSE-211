9fd551d8058b0687b43bd6b4863a8ad9fb75fe16
MoeSamee <47723277+MoeSamee@users.noreply.github.com>
2019-04-01 18:27:10 -0400

    Update AssessCanWeight.java
    Java Doc

--- src/ca/mcgill/ecse211/model/AssessCanWeight.java
+++ src/ca/mcgill/ecse211/model/AssessCanWeight.java
@@ -2,6 +2,13 @@ package ca.mcgill.ecse211.model;
 
 import lejos.robotics.SampleProvider;
 
+/**
+ * This class allows the robot to correctly identify a can's weight by
+ * fetching the touch sensor's sample. 
+ * @author Carlo D'Angelo, Mohamed Samee
+ *
+ */
+
 public class AssessCanWeight {
 
 	private float[] tsData;
@@ -12,10 +19,16 @@ public class AssessCanWeight {
 		this.myTouchStatus = myTouchStatus;
 	}
 	
+	/**
+ 	* This method returns the touch sensor's fetched data casted into an int. 
+ 	*/
 	public int run() {
 		return (int) sampleData();
 	}
 	
+	/**
+	 * This method fetches the data from the touch sensor.
+	 */
 	private float sampleData(){
 		myTouchStatus.fetchSample(tsData, 0);
 		return tsData[0];
