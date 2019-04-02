184423d5d2133230fec64cf374b2e4fe7cf27f8b
Carlo D'Angelo <carlodangelo@outlook.com>
2019-04-02 18:27:37 -0400

    Color Classification java code

--- src/ca/mcgill/ecse211/model/ColorClassification.java
+++ src/ca/mcgill/ecse211/model/ColorClassification.java
@@ -2,8 +2,23 @@ package ca.mcgill.ecse211.model;
 
 import lejos.robotics.SampleProvider;
 
-// testing : return the sensor value and print it on the screen
-
+/**
+ * This class implements color classification to identify the color of the cans. Essentially, it
+ * implements the color sensor to obtain the RGB values of the colored cans. Then, the RGB values
+ * collected will be normalized by the average over all 3 channels, and compared with the normalized
+ * mean value of the color of each can. The color can be identified once the minimum difference in
+ * the RGB values is met.
+ * 
+ * @author1 Ketan Rampurkar
+ * @author2 Hao Shu
+ * @author3 Carlo D'Angelo
+ * @author4 Mohamed Samee
+ * @author5 Marie-Lynn Mansour
+ * @author6 Cristian Ciungu
+ * @version 25-02-2019
+ * 
+ * 
+ */
 public class ColorClassification {
 
   // use an array to collect color data
@@ -11,6 +26,10 @@ public class ColorClassification {
   private SampleProvider colorID;
   private static final double THRESHOLD = 0.35;
 
+  /**
+   * This 2d array stores the mean RGB values for each colored can
+   * 
+   */
   private final float[][] MEAN_RGB = {// the mean RGB values for cans
 	      {0.0551924454f, 0.0069923890f, 0.0339901908f}, // red can
 	      {0.0520519090f, 0.0347073221f, 0.0072014234f}, // yellow can
@@ -18,13 +37,24 @@ public class ColorClassification {
 	      {0.0235257231f, 0.0502034322f, 0.0178382312f}	 // green can
 	  };
 
-  // constructor
+  /**
+   * Constructor to initialize variables
+   * 
+   * @param float[] colorData
+   * @param SampleProvider colorId
+   */
   public ColorClassification(float[] colorData, SampleProvider colorId) {
     this.colorData = colorData;
     this.colorID = colorId;
 
   }
 
+  /**
+   * This method receives the position of the detected color and returns the name of the color as a
+   * String
+   * 
+   * @return String
+   */
   public String run() {
     int a = findColor(sampleData());
 
@@ -38,7 +68,14 @@ public class ColorClassification {
 
   }
 
-
+  /**
+   * This method normalizes the RGB data collected by the color sensor, and then compare them with
+   * the normalized mean RGB values one by one. If the minimum difference is met, the position of
+   * the detected color in the 2d array will be returned as an int
+   * 
+   * @param colorData
+   * @return i : int
+   */
   private int findColor(float[] colorData) {
     float eucDistance = (float) Math.sqrt((Math.pow(colorData[0], 2) + Math.pow(colorData[1], 2) + Math.pow(colorData[2], 2)));
 
@@ -62,6 +99,11 @@ public class ColorClassification {
     return 4;
   }
   
+  /**
+   * This method is used to fetch the RGB values from the color sensor and store them in an array
+   * 
+   * @return colorData : float[]
+   */
   private float[] sampleData(){
   colorID.fetchSample(colorData, 0);
   return colorData;
