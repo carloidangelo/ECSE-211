e2e74d5cb8f7729d691f3f0e64c89b2423c67734
Carlo D'Angelo <carlodangelo@outlook.com>
2019-04-06 14:22:47 -0400

    javadoc

--- src/ca/mcgill/ecse211/model/LightLocalizer.java
+++ src/ca/mcgill/ecse211/model/LightLocalizer.java
@@ -18,6 +18,10 @@ public class LightLocalizer {
   private static final int FORWARD_SPEED = 150; 
   private final double TILE_SIZE = Navigation.TILE_SIZE;
   private static final int COLOUR_DIFF = 20;  
+  
+  /**
+   * Distance (cm) between the color sensor and the axis of rotation of the robot.
+   */
   private static final double LIGHT_LOC_DISTANCE = 10;
   private static final int EXTRA_DISTANCE = 5;
   private static final double TURN_ERROR = 15.7; 
